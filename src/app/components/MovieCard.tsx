4d9d917e0a34819b5e8db983c3af3b9cffbe2eae
25LP5852 25LP5852 <Ochrbt0@icloud.com>
2025-12-23 12:35:17 +0800

    second

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -1,6 +1,4 @@
-import { title } from "process";
-
-import React from "react";
+import { log } from "console";
 
 export type Movie = {
   title: string;
@@ -9,23 +7,44 @@ export type Movie = {
   image: string;
 };
 
-const sampleMovie: Movie[] = [
-  {
-    title: "Dear Santa",
-    rating: 6.9,
+// const sampleMovie: Movie[] = [
+// ];
 
-    od: "./Star.png",
-    image: "./dearsanta.jpg",
-  },
-  {
-    title: "How To Train Your Dragon Live Action",
-    rating: 6.9,
+const movieAPI = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US@page=1",
+    {
+      headers: {
+        "Content-type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTUxNTYwYjVhNGM5ZDI5OTNjYmQxZDIzNjg3MzM4MiIsIm5iZiI6MTc2NjQ2MjAzNy40MDUsInN1YiI6IjY5NGExMjU1MTk4OWM2YzQyNzhjZWI1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O5EHu68s0ueTRbeCfYYDgANOsPMyX1rlU_pO5O_CAAw",
+      },
+    }
+  );
 
-    od: "",
-    image: "",
-  },
-];
+  const data = await response.json();
 
-export const MovieCard = () => {
-  return <div></div>;
+  return data;
+};
+
+export const MovieCard = async () => {
+  const movies = await movieAPI();
+  console.log(movies);
+  return (
+    <div className="flex space-x-8">
+      {/* {sampleMovie.map((movie) => (
+        <div
+          key={movie.title}
+          className="w-[229.73px] h-109.75  bg-gray-50  space-y-1"
+        >
+          <img src={movie.image} alt="" className="w-[229.73px] h-85" />
+          <p className="flex items-center">
+            <img src={movie.od} alt="" className="w-4 h-4.5 pr-1 pb-1.25" />
+            {movie.rating}/10
+          </p>
+          <p className="text-lg">{movie.title}</p>
+        </div>
+      ))} */}
+    </div>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 import { MovieCard } from "./components/MovieCard";
 import { Scroll } from "./components/Scroll";
 
+const sampleMovie = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/upcoming?language=en-US@page=1",
+    {
+      headers: {
+        "Content-type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTUxNTYwYjVhNGM5ZDI5OTNjYmQxZDIzNjg3MzM4MiIsIm5iZiI6MTc2NjQ2MjAzNy40MDUsInN1YiI6IjY5NGExMjU1MTk4OWM2YzQyNzhjZWI1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O5EHu68s0ueTRbeCfYYDgANOsPMyX1rlU_pO5O_CAAw",
+      },
+    }
+  );
+};
+
 export default function Home() {
   return (
-    <div className="w-full bg-red-50">
+    <div className="w-full">
       <Scroll />
-      <MovieCard />
+      <div className="flex flex-col">
+        <div className="flex pt-13">
+          <p className="text-2xl">Upcoming</p>
+          <button>see more</button>
+        </div>
+        <MovieCard />
+      </div>
     </div>
   );
 }
